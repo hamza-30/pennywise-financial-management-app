8dcf69e62e1efaf2030a5d5b7a71181e2f5387ab
Muhammad Hamza <hamzahs2004@gmail.com>
2026-01-22 22:02:45 +0500

    feat(transactions): add responsive card and row components

---
+++ src/components/TransactionCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FiEdit2 } from "react-icons/fi";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+function TransactionCard({ desc, type, date, category, amount }) {
+  return (
+    <div
+      className={`md:hidden w-full h-fit mt-7 rounded-xl border border-gray-100 px-5 py-5 font-[sans-serif] shadow-[0_0px_10px_rgba(0,0,0,0.04)]`}
+    >
+      <div
+        className={`flex justify-between border-b border-gray-100 pb-4 mb-5`}
+      >
+        <div className={`flex flex-col `}>
+          <span className={`text-[1.05rem] font-semibold text-[#111e43]`}>
+            {desc}
+          </span>
+          <span className={`text-xs text-gray-500`}>{date}</span>
+        </div>
+
+        {type == "Income" ? (
+          <div className={`font-semibold text-[1.04rem] text-[#33a887]`}>
+            +{amount}
+          </div>
+        ) : (
+          <div className={`font-semibold text-[1.04rem] text-[#111e43]`}>
+            -{amount}
+          </div>
+        )}
+      </div>
+
+      <div className={`flex justify-between items-center`}>
+        <span
+          className={`h-fit text-xs text-gray-600 bg-[#f0eeee] px-2 py-1 rounded-lg`}
+        >
+          {category}
+        </span>
+        <div>
+          <button
+            className={`text-[#c4f82a] text-[1.1rem] bg-[#f2fdd3] active:bg-[#e6fcac] inline-flex mr-3 px-1.5 py-1.5 rounded-md lg:text-gray-500 lg:bg-gray-100 hover:text-[#c4f82a] hover:bg-[#f3fcda]`}
+          >
+            <FiEdit2 />
+          </button>
+          <button
+            className={`text-[#ab4f55] text-[1.1rem] bg-[#fee4e6] active:bg-[#ffcdd1] inline-flex px-1.5 py-1.5 rounded-md`}
+          >
+            <RiDeleteBinLine />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TransactionCard;
---
+++ src/components/TransactionRow.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FiEdit2 } from "react-icons/fi";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+function TransactionRow({ desc, type, category, date, amount }) {
+  return (
+    <>
+      <tr className="hover:bg-gray-50 hidden md:table-row">
+        <td className="px-3 py-4 text-black">{desc}</td>
+        <td className="px-3 py-4">
+          {type == "Expense" ? (
+            <span
+              className={`text-[0.8rem] text-center text-[#ab4f55] font-semibold bg-[#fae5e6] px-3 pb-0.5 rounded-xl`}
+            >
+              expense
+            </span>
+          ) : (
+            <span
+              className={`text-[0.8rem] text-center text-[#428176] font-semibold bg-[#bef9ef] px-3 pb-0.5 rounded-xl`}
+            >
+              income
+            </span>
+          )}
+        </td>
+        <td className="px-3 py-4 text-[#777e86]">{category}</td>
+        <td className="px-3 py-4 text-[#777e86]">{date}</td>
+        <td className="px-3 py-4 text-right text-[1.06rem] font-semibold">
+          {type == "Income" ? `+${amount}` : `-${amount}`}
+        </td>
+        <td className="px-3 py-4 text-right">
+          <button
+            className={`text-[#c4f82a] text-[1.1rem] bg-[#f2fdd3] inline-flex mr-3 px-1 py-1 rounded-md lg:text-gray-500 lg:bg-gray-100 hover:text-[#c4f82a] hover:bg-[#f3fcda]`}
+          >
+            <FiEdit2 />
+          </button>
+          <button
+            className={`text-[#ab4f55] text-[1.1rem] bg-[#fee4e6] inline-flex px-1 py-1 rounded-md lg:text-gray-500 lg:bg-gray-100 hover:text-[#ab4f55] hover:bg-[#fee4e6]`}
+          >
+            <RiDeleteBinLine />
+          </button>
+        </td>
+      </tr>
+    </>
+  );
+}
+
+export default TransactionRow;
