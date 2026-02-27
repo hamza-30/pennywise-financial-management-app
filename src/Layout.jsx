0645052befb30117a92941070a2da3d5f480c886
Muhammad Hamza <hamzahs2004@gmail.com>
2026-02-27 17:51:21 +0500

    feat(Layout): add scroll reset on pathname change for improved navigation experience

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from "./components/Sidebar"
 import Header from "./components/Header"
 
 function Layout() {
+  const {pathname} = useLocation()
+  const scrollRef = useRef(null)
+
+  useEffect(() => {
+    if(scrollRef.current){
+      scrollRef.current.scrollTop = 0
+    }
+  }, [pathname])
+
   return (
     <div className='w-full h-screen flex overflow-hidden'>
         <Sidebar />
 
-        <div className='flex flex-col flex-1  overflow-y-auto'>
+        <div ref={scrollRef} className='flex flex-col flex-1  overflow-y-auto'>
           <Header />
           <main className={`flex-1`}>
             <Outlet />
