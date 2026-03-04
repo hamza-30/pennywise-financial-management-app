4c90fb3d41ae1f49e1f343eeea07c817a1b71324
Muhammad Hamza <hamzahs2004@gmail.com>
2026-03-05 02:00:11 +0500

    feat(TransactionContextProvider): implement Firestore integration for transaction management

--- src/context/TransactionContext/TransactionContextProvider.jsx
+++ src/context/TransactionContext/TransactionContextProvider.jsx
@@ -1,24 +1,90 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TransactionContext from "./TransactionContext";
 import { useContext } from "react";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  where,
+  doc,
+} from "firebase/firestore";
+import { useAuthContext } from "../AuthContext/AuthContextProvider";
+import { db } from "../../firebase/firebase";
 
 function TransactionContextProvider({ children }) {
   const [transactions, setTransactions] = useState([]);
+  const { user } = useAuthContext();
 
-  function addTransaction(newTransaction) {
-    setTransactions((prev) => [...prev, newTransaction]);
+  useEffect(() => {
+    if (!user) {
+      setTransactions([]);
+      return;
+    }
+
+    const colRef = collection(db, "transactions");
+    const q = query(colRef, where("userId", "==",  user.uid));
+
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot) => {
+        const remoteTransactions = snapshot.docs.map((document) => ({
+          id: document.id,
+          ...document.data(),
+        }));
+
+        setTransactions(remoteTransactions);
+      },
+      (error) => {
+        console.error("Firestore Listener Error:", error.message);
+      },
+    );
+
+    return () => unsubscribe();
+  }, [user]);
+
+  async function addTransaction(newTransaction) {
+    try {
+      await addDoc(collection(db, "transactions"), {...newTransaction,
+        userId: user.uid
+      })
+
+    } catch (error) {
+      console.error("Error adding transaction:", error)
+    }
   }
 
-  function deleteTransaction(id) {
-    setTransactions((prev) => prev.filter((trans) => trans.id !== id));
+  async function deleteTransaction(id) {
+    try {
+      const docRef = doc(db, "transactions", id)
+      await deleteDoc(docRef)
+
+    } catch (error) {
+      console.error("Error deleting transaction:", error)
+    }
   }
 
-  function editTransaction(id, updatedTransaction){
-    setTransactions((prev) => prev.map((trans) => trans.id == id ? {...trans, ...updatedTransaction} : trans))
+  async function editTransaction(id, updatedTransaction) {
+    try {
+      const docRef = doc(db, "transactions", id)
+      await updateDoc(docRef, updatedTransaction)
+
+    } catch (error) {
+      console.error("Error deleting transaction:", error)
+    }
   }
 
   return (
-    <TransactionContext.Provider value={{ transactions, addTransaction, deleteTransaction, editTransaction }}>
+    <TransactionContext.Provider
+      value={{
+        transactions,
+        addTransaction,
+        deleteTransaction,
+        editTransaction,
+      }}
+    >
       {children}
     </TransactionContext.Provider>
   );
