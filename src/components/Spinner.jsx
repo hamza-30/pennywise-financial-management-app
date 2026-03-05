ee83aa92be2d646d90c6cb9da215d4ad6d9c7f9e
Muhammad Hamza <hamzahs2004@gmail.com>
2026-03-06 01:27:24 +0500

    feat(Spinner): add fullPage prop for customizable spinner size and positioning

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Spinner({ message = "Loading"}) {
+function Spinner({ message = "Loading", fullPage = "false"}) {
   return (
-    <div className={`flex flex-col items-center justify-center gap-y-3 h-full w-full`}>
+    <div className={`flex flex-col items-center justify-center gap-y-3 ${fullPage ? "h-screen w-screen": "h-full w-full"}`}>
       <div 
-        className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-[#c4f82a] relative bottom-10"
+        className={`h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-[#c4f82a] relative ${fullPage ? "bottom-0" : "bottom-10"}`}
       ></div>
       
-      <span className="text-sm font-semibold tracking-wide text-gray-500 uppercase relative bottom-10">
+      <span className={`text-sm font-semibold tracking-wide text-gray-500 uppercase relative ${fullPage ? "bottom-0" : "bottom-10"}`}>
         {message}
       </span>
     </div>
