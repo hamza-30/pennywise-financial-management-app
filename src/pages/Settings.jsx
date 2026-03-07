faeae2f0bfbb074bfa8889208620e22bc32ad0c6
Muhammad Hamza <hamzahs2004@gmail.com>
2026-03-07 19:33:35 +0500

    style(Settings): add ring to export and delete buttons

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,40 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { LuShield } from "react-icons/lu";
 import { FiDownload } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { TbAlertTriangle } from "react-icons/tb";
-import { useTransactions } from '../context/TransactionContext/TransactionContextProvider';
+import { useTransactions } from "../context/TransactionContext/TransactionContextProvider";
 
 function Settings() {
-    const [warningModalOpen, setWarningModalOpen] = useState(false)
-    const [isDeleting, setIsDeleting] = useState(false)
-    const { deleteAllTransactions } = useTransactions()
+  const [warningModalOpen, setWarningModalOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const { deleteAllTransactions } = useTransactions();
 
-    async function handleDeleteClick(){
-        setIsDeleting(true)
+  async function handleDeleteClick() {
+    setIsDeleting(true);
 
-        try {
-            await deleteAllTransactions()
-            setWarningModalOpen(false)
-        } catch (error) {
-            console.error("Error deleting data:", error)
-        }
-          finally{
-            setIsDeleting(false)
-        }
+    try {
+      await deleteAllTransactions();
+      setWarningModalOpen(false);
+    } catch (error) {
+      console.error("Error deleting data:", error);
+    } finally {
+      setIsDeleting(false);
     }
+  }
 
-    function closeWarningModal(){
-        if(!isDeleting){
-            setWarningModalOpen(false)
-        }
+  function closeWarningModal() {
+    if (!isDeleting) {
+      setWarningModalOpen(false);
     }
+  }
 
   return (
     <>
-    <div
+      <div
         className={`flex flex-col h-full gap-y-8 md:gap-y-6 pt-8 pb-5 px-4 lg:px-8 overflow-hidden`}
-    >
+      >
         <div
           className={`h-12 flex gap-y-4 justify-between items-center text-[#383f45]`}
         >
@@ -42,43 +41,62 @@ function Settings() {
         </div>
 
         <div
-            className={`w-full flex flex-col px-6 py-5 rounded-xl shadow-[0_0px_8px_rgba(0,0,0,0.04)]
+          className={`w-full flex flex-col px-6 py-5 rounded-xl shadow-[0_0px_8px_rgba(0,0,0,0.04)]
                   bg-white gap-y-6`}
-          >
-            <div className={`flex items-center gap-x-2 text-[#383f45] mb-1`}>
-                <LuShield className='text-xl'/>
-                <span className={`text-lg font-semibold`}>Data & Privacy</span>
-            </div> 
+        >
+          <div className={`flex items-center gap-x-2 text-[#383f45] mb-1`}>
+            <LuShield className="text-xl" />
+            <span className={`text-lg font-semibold`}>Data & Privacy</span>
+          </div>
 
-            <div className={`flex justify-between items-center`}>
-                <div className={`flex flex-col w-1/2 md:w-fit`}>
-                    <span className={`text-base md:text-[1.03rem] leading-tight font-medium text-[#3b3a3a]`}>Export Data</span>
-                    <span className={`text-[0.8rem] md:text-sm text-gray-500`}>Download your transaction history</span>
-                </div>
-                <div className={`flex items-center justify-center px-2 md:w-30 h-7.5 rounded-lg gap-x-2 bg-[#292929] hover:bg-[#292929c3] active:bg-[#292929c3] text-white cursor-pointer`}>
-                    <FiDownload className={`text-base md:text-lg`}/>
-                    <span className={`text-sm md:text-[0.95rem] font-medium`}>Export CSV</span>
-                </div>
+          <div className={`flex justify-between items-center`}>
+            <div className={`flex flex-col w-1/2 md:w-fit`}>
+              <span
+                className={`text-base md:text-[1.03rem] leading-tight font-medium text-[#3b3a3a]`}
+              >
+                Export Data
+              </span>
+              <span className={`text-[0.8rem] md:text-sm text-gray-500`}>
+                Download your transaction history
+              </span>
             </div>
-
-            <div className={`flex justify-between items-center`}>
-                <div className={`flex flex-col w-1/2 md:w-fit`}>
-                    <span className={`text-base md:text-[1.03rem] leading-tight font-medium text-red-500`}>Clear Data</span>
-                    <span className={`text-[0.8rem] md:text-sm text-gray-500`}>Permanently remove all transactions</span>
-                </div>
-                <div
-                 onClick={() => setWarningModalOpen(true)}
-                 className={`flex items-center justify-center px-2 md:w-30 h-7.5 rounded-lg gap-x-2 bg-red-50 hover:bg-red-100 active:bg-red-100 cursor-pointer text-red-500`}>
-                    <RiDeleteBin6Line className={`text-base md:text-lg`}/>
-                    <span className={`text-sm md:text-[0.95rem] font-medium`}>Delete All</span>
-                </div>
+            <div
+              className={`flex items-center justify-center px-2 md:w-30 h-7.5 rounded-lg gap-x-2 bg-[#292929] hover:bg-[#292929c3] active:bg-[#292929c3] text-white cursor-pointer transition-all ease-in duration-100
+                   active:ring-[#292929] active:ring-[0.15rem] active:ring-offset-2`}
+            >
+              <FiDownload className={`text-base md:text-lg`} />
+              <span className={`text-sm md:text-[0.95rem] font-medium`}>
+                Export CSV
+              </span>
             </div>
           </div>
 
-          
-    </div>
+          <div className={`flex justify-between items-center`}>
+            <div className={`flex flex-col w-1/2 md:w-fit`}>
+              <span
+                className={`text-base md:text-[1.03rem] leading-tight font-medium text-red-500`}
+              >
+                Clear Data
+              </span>
+              <span className={`text-[0.8rem] md:text-sm text-gray-500`}>
+                Permanently remove all transactions
+              </span>
+            </div>
+            <div
+              onClick={() => setWarningModalOpen(true)}
+              className={`flex items-center justify-center px-2 md:w-30 h-7.5 rounded-lg gap-x-2 bg-red-50 hover:bg-red-100 active:bg-red-100 cursor-pointer text-red-500 transition-all ease-in duration-100
+                 active:ring-[0.15rem] active:ring-offset-2`}
+            >
+              <RiDeleteBin6Line className={`text-base md:text-lg`} />
+              <span className={`text-sm md:text-[0.95rem] font-medium`}>
+                Delete All
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-    <div
+      <div
         className={`inset-0 z-40
         ${warningModalOpen ? "fixed" : "hidden"}`}
         onClick={closeWarningModal}
@@ -90,10 +108,12 @@ function Settings() {
                 `}
       >
         <div className={`flex flex-col items-center text-red-500`}>
-            <TbAlertTriangle className={`text-5xl`}/>
-            <p className={`font-medium`}>Danger Zone</p>
-        </div>  
-        <p className={`font-medium`}>Once you delete your data, there is no going back. Please be certain.</p>
+          <TbAlertTriangle className={`text-5xl`} />
+          <p className={`font-medium`}>Danger Zone</p>
+        </div>
+        <p className={`font-medium`}>
+          Once you delete your data, there is no going back. Please be certain.
+        </p>
 
         <div className={`ml-auto`}>
           <button
@@ -112,8 +132,8 @@ function Settings() {
           </button>
         </div>
       </div>
-      </>
-  )
+    </>
+  );
 }
 
-export default Settings
+export default Settings;
