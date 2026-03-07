47719cfcd482b233fa8135fb71e911bf84bfb114
Muhammad Hamza <hamzahs2004@gmail.com>
2026-03-07 19:22:52 +0500

    feat(Settings): implement deletion of all data with warning modal

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LuShield } from "react-icons/lu";
 import { FiDownload } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
-
+import { TbAlertTriangle } from "react-icons/tb";
+import { useTransactions } from '../context/TransactionContext/TransactionContextProvider';
 
 function Settings() {
+    const [warningModalOpen, setWarningModalOpen] = useState(false)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const { deleteAllTransactions } = useTransactions()
+
+    async function handleDeleteClick(){
+        setIsDeleting(true)
+
+        try {
+            await deleteAllTransactions()
+            setWarningModalOpen(false)
+        } catch (error) {
+            console.error("Error deleting data:", error)
+        }
+          finally{
+            setIsDeleting(false)
+        }
+    }
+
+    function closeWarningModal(){
+        if(!isDeleting){
+            setWarningModalOpen(false)
+        }
+    }
+
   return (
+    <>
     <div
         className={`flex flex-col h-full gap-y-8 md:gap-y-6 pt-8 pb-5 px-4 lg:px-8 overflow-hidden`}
     >
@@ -40,15 +66,53 @@ function Settings() {
                     <span className={`text-base md:text-[1.03rem] leading-tight font-medium text-red-500`}>Clear Data</span>
                     <span className={`text-[0.8rem] md:text-sm text-gray-500`}>Permanently remove all transactions</span>
                 </div>
-                <div className={`flex items-center justify-center px-2 md:w-30 h-7.5 rounded-lg gap-x-2 bg-red-50 hover:bg-red-100 active:bg-red-100 cursor-pointer text-red-500`}>
+                <div
+                 onClick={() => setWarningModalOpen(true)}
+                 className={`flex items-center justify-center px-2 md:w-30 h-7.5 rounded-lg gap-x-2 bg-red-50 hover:bg-red-100 active:bg-red-100 cursor-pointer text-red-500`}>
                     <RiDeleteBin6Line className={`text-base md:text-lg`}/>
                     <span className={`text-sm md:text-[0.95rem] font-medium`}>Delete All</span>
                 </div>
             </div>
           </div>
 
-
+          
     </div>
+
+    <div
+        className={`inset-0 z-40
+        ${warningModalOpen ? "fixed" : "hidden"}`}
+        onClick={closeWarningModal}
+      ></div>
+      <div
+        className={`top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                w-[90vw] md:w-[60vw] lg:w-120 pt-4 pb-3 px-4 md:px-6 backdrop-blur-xl flex flex-col items-center bg-white rounded-2xl border border-gray-600 transition-all duration-300 ease-in-out z-50 gap-y-5
+                ${warningModalOpen ? "fixed" : "hidden"}
+                `}
+      >
+        <div className={`flex flex-col items-center text-red-500`}>
+            <TbAlertTriangle className={`text-5xl`}/>
+            <p className={`font-medium`}>Danger Zone</p>
+        </div>  
+        <p className={`font-medium`}>Once you delete your data, there is no going back. Please be certain.</p>
+
+        <div className={`ml-auto`}>
+          <button
+            className={`px-3 py-1.5 text-sm mr-3 hover:bg-gray-100 active:bg-gray-100 rounded-lg cursor-pointer`}
+            onClick={closeWarningModal}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            disabled={isDeleting}
+            className={`min-w-[11rem] py-1.5 text-sm w-fit text-white rounded-lg bg-[#ef4444] hover:bg-[#dc2626] active:bg-[#dc2626] cursor-pointer`}
+            onClick={handleDeleteClick}
+          >
+            {isDeleting ? "Deleting everything..." : "Delete All Transactions"}
+          </button>
+        </div>
+      </div>
+      </>
   )
 }
 
